3b5423b4982e18a8afbb090e590925a2b70d738f
Al Uretsky <auretsky1@yahoo.com>
2015-03-30 23:09:55 -0400

    Added boiler-plate to bring up pygame window.

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+pygame.display.set_mode()
+(width, height) = (400, 400)
+screen = pygame.display.set_mode((width, height))
+pygame.display.flip()
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+
+
 if __name__ == "__main__":
     print("Pong")
+    print("Python3 Game")
